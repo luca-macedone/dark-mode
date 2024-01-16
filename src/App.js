fd7556ce6b8320c1cb45984b00e2990bc80c0d5b
luca <luca.macedone@outlook.it>
2024-01-16 18:06:15 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const changeTheme = () => {
+    if (theme === "light") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+    console.log(document.documentElement.dataset);
+  };
+
+  useEffect(() => {
+    if (theme === "light") {
+      document.documentElement.dataset.bsTheme = "light";
+    } else {
+      document.documentElement.dataset.bsTheme = "dark";
+    }
+    console.log("theme => ", theme);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <button
+              type="button"
+              className="btn btn-dark rounded-pill"
+              onClick={changeTheme}
+            >
+              Cambia
+            </button>
+          </div>
+        </div>
+        <div className="row row-cols-1 row-cols-md-2">
+          <div className="col">
+            Sint nisi dolore sunt nostrud do incididunt nostrud commodo aliqua
+            dolor.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
