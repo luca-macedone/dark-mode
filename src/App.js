a64cc8ea00b4cd7a420fa1adfaf92a8773bb654c
luca <luca.macedone@outlook.it>
2024-01-17 10:05:18 +0100

    add theme mode switch and relative css rules

--- src/App.js
+++ src/App.js
@@ -1,46 +1,94 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+const data = [
+  {
+    title: "lorem ipsum",
+    description:
+      "Sunt eu fugiat laboris Lorem occaecat cupidatat ad irure elit enim veniam consectetur. Sint ea duis magna Lorem consequat dolor nulla. Laborum incididunt et aliquip qui.",
+  },
+  {
+    title: "amet dolor",
+    description:
+      "Sunt eu fugiat laboris Lorem occaecat cupidatat ad irure elit enim veniam consectetur. Sint ea duis magna Lorem consequat dolor nulla. Laborum incididunt et aliquip qui.",
+  },
+  {
+    title: "quit est",
+    description:
+      "Sunt eu fugiat laboris Lorem occaecat cupidatat ad irure elit enim veniam consectetur. Sint ea duis magna Lorem consequat dolor nulla. Laborum incididunt et aliquip qui.",
+  },
+  {
+    title: "eum et est",
+    description:
+      "Sunt eu fugiat laboris Lorem occaecat cupidatat ad irure elit enim veniam consectetur. Sint ea duis magna Lorem consequat dolor nulla. Laborum incididunt et aliquip qui.",
+  },
+  {
+    title: "nesciunt quas odio",
+    description:
+      "Sunt eu fugiat laboris Lorem occaecat cupidatat ad irure elit enim veniam consectetur. Sint ea duis magna Lorem consequat dolor nulla. Laborum incididunt et aliquip qui.",
+  },
+];
+
 function App() {
   const [theme, setTheme] = useState("light");
 
-  const changeTheme = () => {
+  useEffect(() => {
+    document.documentElement.dataset.bsTheme = "light";
+    setTheme("light");
+  }, []);
+
+  const handleTheme = () => {
     if (theme === "light") {
-      setTheme("light");
-    } else {
       setTheme("dark");
+    } else {
+      setTheme("light");
     }
-    console.log(document.documentElement.dataset);
+    changeTheme();
+    // console.log(theme);
   };
 
-  useEffect(() => {
-    if (theme === "light") {
-      document.documentElement.dataset.bsTheme = "light";
-    } else {
+  const changeTheme = () => {
+    let docData = document.documentElement.dataset.bsTheme;
+    if (docData === "light") {
       document.documentElement.dataset.bsTheme = "dark";
+    } else {
+      document.documentElement.dataset.bsTheme = "light";
     }
-    console.log("theme => ", theme);
-  });
+  };
 
   return (
     <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col">
+      <div className="container-sm">
+        <div className="row py-3">
+          <div className="col d-flex align-items-center justify-content-center">
             <button
               type="button"
-              className="btn btn-dark rounded-pill"
-              onClick={changeTheme}
+              className="btn rounded-pill py-1 px-4 text-uppercase fw-semibold"
+              onClick={handleTheme}
             >
               Cambia
             </button>
           </div>
         </div>
-        <div className="row row-cols-1 row-cols-md-2">
-          <div className="col">
-            Sint nisi dolore sunt nostrud do incididunt nostrud commodo aliqua
-            dolor.
-          </div>
+        <div className="row row-cols-1 row-cols-md-2 g-4 py-5">
+          {data.map((elem, index) => {
+            return (
+              <>
+                <div
+                  key={index}
+                  className="col"
+                >
+                  <div className="card card-body rounded-3 border-0 shadow px-4">
+                    <h5 className="fw-bold text-capitalize text-center">
+                      {elem.title}
+                    </h5>
+                    <hr className="w-25 mx-auto mt-0 mb-3"></hr>
+                    <p className="text-center">{elem.description}</p>
+                  </div>
+                </div>
+              </>
+            );
+          })}
         </div>
       </div>
     </div>
